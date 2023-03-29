9dede03635ddff1d9196de57db0811ba1f1cf96e
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-29 21:37:42 +0200

    feat: display of search results

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,9 +1,23 @@
 <script>
+// importo lo store
+import { store } from '../Store';
+
 export default {
-    name: 'MainApp'
+    name: 'MainApp',
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 
 <template>
-    <div>results</div>
+    <!-- stampo in pagina i dati dei film trovati -->
+    <div v-for="result in store.movieResults">
+        <h2>Titolo: {{ result.title }}</h2>
+        <h3>Titolo originale: {{ result.original_title }}</h3>
+        <p>Lingua: {{ result.original_language }}</p>
+        <p>Voto: {{ result.vote_average }}</p>
+    </div>
 </template>
