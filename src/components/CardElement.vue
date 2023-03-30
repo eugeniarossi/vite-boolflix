f37dab0a7bdceeddab1c86d976037fb75740a032
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-30 13:54:44 +0200

    feat: import flags package

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -1,8 +1,13 @@
 <script>
+import CountryFlag from 'vue-country-flag-next'
+
 export default {
     name: 'CardElement',
     props: {
         info: Object
+    },
+    components: {
+        CountryFlag
     }
 }
 </script>
@@ -14,6 +19,7 @@ export default {
         <h4>{{ info.original_title }}</h4>
         <div>{{ info.original_language }}</div>
         <div>{{ info.vote_average }}</div>
+        <country-flag country='fr' size='small'/>
     </div>
     <!-- /card -->
 </template>
